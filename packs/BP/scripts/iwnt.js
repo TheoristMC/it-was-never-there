82e7666bb79033c0e6dae2e98f7cba0fdfc218b9
TheoristMC <zunheorist@gmail.com>
2025-04-06 09:16:32 +0800

    Demo: Item Name Distortion

--- packs/BP/scripts/iwnt.js
+++ packs/BP/scripts/iwnt.js
@@ -1,1 +1,48 @@
-import { world } from "@minecraft/server";
+import { EntityComponentTypes, system, TicksPerSecond, world } from "@minecraft/server";
+
+function generateRandomTick(min = 10, max = 20) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return TicksPerSecond * randomNumber;
+}
+
+function getRandomElementInArray(arr) {
+  const index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
+function nameSortAlphabetically(itemName) {
+  return itemName
+    .replace("minecraft:", "")
+    .replaceAll("_", " ")
+    .split(" ")
+    .map(word => {
+      const [first, ...rest] = word;
+      return [first.toUpperCase(), ...rest].sort().join("");
+    })
+    .join(" ");
+}
+
+const tickSelected = generateRandomTick();
+world.sendMessage(`${tickSelected / 20} seconds is selected.`);
+
+system.runInterval(() => {
+  world.getAllPlayers().forEach((player) => {
+    const inventory = player.getComponent(EntityComponentTypes.Inventory).container;
+    const itemHasBeenSelectedSuffix = "§h§a§s§r";
+    const itemsArray = [];
+
+    for (let i = 0; i < inventory.size; i++) {
+      const item = inventory?.getItem(i);
+      if (!(item || item?.nameTag?.includes(itemHasBeenSelectedSuffix))) continue;
+
+      itemsArray.push({
+        slot: i,
+        item
+      });
+    }
+
+    const { item, slot } = getRandomElementInArray(itemsArray);
+    item.nameTag = nameSortAlphabetically(item.typeId) + itemHasBeenSelectedSuffix;
+    inventory.setItem(slot, item);
+  });
+}, tickSelected);
