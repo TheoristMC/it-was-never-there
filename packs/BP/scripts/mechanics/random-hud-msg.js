33bc924243c86a792a2ddbfcbfc8861a688d249e
TheoristMC <zunheorist@gmail.com>
2025-04-14 11:44:01 +0800

    Implemented the basic technical of Random HUD Messages

--- packs/BP/scripts/mechanics/random-hud-msg.js
+++ packs/BP/scripts/mechanics/random-hud-msg.js
@@ -1,4 +1,9 @@
+import { world } from "@minecraft/server";
+import { getRandomElementInArray, getRandomInt } from "../iwnt";
+
 const randomHUDMsgProps = {
+  lastMessageShown: new Map(),
+  maxLastMessageToStore: 3,
   messages: [
     "You've already done this.",
     "It's not real.",
@@ -10,6 +15,32 @@ const randomHUDMsgProps = {
     "You didn't name this, right?",
     "This block wasn't here.",
     "Just keep going.",
+    "You did this to yourself.",
     "Everything is fine.",
   ],
 };
+
+world.afterEvents.itemUse.subscribe((data) => {
+  const { itemStack, source } = data;
+
+  if (itemStack.typeId === "minecraft:carrot") {
+    const lastMessages =
+      randomHUDMsgProps.lastMessageShown.get(source.id) || [];
+    const filteredMessages = randomHUDMsgProps.messages.filter(
+      (msg) => !lastMessages.includes(msg)
+    );
+
+    const randomMessage = getRandomElementInArray(filteredMessages);
+    const randomOffsetType = getRandomInt(1, 5);
+
+    source.onScreenDisplay.setTitle(
+      `iwnt_msg_popup:${randomOffsetType}:${randomMessage}`
+    );
+
+    lastMessages.push(randomMessage);
+    if (lastMessages.length > randomHUDMsgProps.maxLastMessageToStore)
+      lastMessages.shift();
+
+    randomHUDMsgProps.lastMessageShown.set(source.id, lastMessages);
+  }
+});
